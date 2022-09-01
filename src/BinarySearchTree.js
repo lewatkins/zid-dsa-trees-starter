35c52d53282464cda136e195b937037e3031b48f
Lorenzo Watkins <lwatkins89@comcast.net>
2022-09-01 13:46:15 -0400

    second commit

--- src/BinarySearchTree.js
+++ src/BinarySearchTree.js
@@ -1,3 +1,5 @@
+const Queue = require("./Queue");
+
 class BinarySearchTree {
 	// your code here
 	constructor(key = null, value = null, parent = null) {
@@ -171,6 +173,26 @@ class BinarySearchTree {
 
     // Finally, process the current node
     values.push(this.value);
+
+    return values;
+  }
+
+  bfs(tree, values = []) {
+    const queue = new Queue();
+    queue.enqueue(tree); // Start the traversal at the tree and add the tree node to the queue to kick off the BFS
+    let node = queue.dequeue(); // Remove from the queue
+    while (node) {
+      values.push(node.value); // Add that value from the queue to an array
+
+      if (node.left) {
+        queue.enqueue(node.left); // Add the left child to the queue
+      }
+
+      if (node.right) {
+        queue.enqueue(node.right); // Add the right child to the queue
+      }
+      node = queue.dequeue();
+    }
 
     return values;
   }
